d14e427511e407d7ac8af6639c2a3c670b36be29
Hendry Widyanto <hendrywidyanto97@gmail.com>
2023-10-15 12:30:24 -0700

    First commit

---
+++ main.py
@@ -0,0 +1,114 @@
+from fastapi import FastAPI, Query
+from fastapi.middleware.cors import CORSMiddleware
+from typing import Annotated
+from pydantic import BaseModel
+from datetime import date
+from sf_api import API
+import requests
+import pandas as pd
+
+
+app = FastAPI()
+
+origins = [
+    "http://localhost",
+    "https://localhost",
+    "http://localhost:5173",
+    "https://localhost:5173",
+    "http://localhost:4173",
+    "http://localhost:4173",
+    "https://abbottreport.netlify.app/",
+    "http://abbottreport.netlify.app/",
+    "*",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+fields = {'product_segment': {'object': 'Product_Segment__c',
+                              'field': 'Name',
+                              'conditions': []},
+          'rdc_code': {'object': 'CMPL123CME__Complaint_Code__c',
+                       'field': 'Name',
+                       'conditions': ["CMPL123CME__Type__c IN ('DEVICE CODE')"]},
+          'rdc_clarifier': {'object': 'CMPL123CME__Complaint_Code__c',
+                            'field': 'Clarifier__c',
+                            'conditions': ["CMPL123CME__Type__c IN ('DEVICE CODE')"]},
+          'pc_code': {'object': 'CMPL123CME__Complaint_Code__c',
+                      'field': 'Name',
+                      'conditions': ["CMPL123CME__Type__c IN ('HEALTH EFFECT - CLINICAL CODE', 'HEALTH EFFECT - IMPACT CODE')"]},
+          'result_code': {'object': 'CMPL123CME__Complaint_Code__c',
+                          'field': 'Name',
+                          'conditions': ["CMPL123CME__Type__c IN ('EVAL RESULT CODE')"]},
+          'conclusion_code': {'object': 'CMPL123CME__Complaint_Code__c',
+                             'field': 'Name',
+                             'conditions': ["CMPL123CME__Type__c IN ('EVAL CONCLUSION CODE')"]},
+          'country': {'object': 'Country__c',
+                      'field': 'Country_Name__c',
+                      'conditions': []}
+         }
+
+@app.get('/fields/{field_name}')
+async def field(field_name: str | None = None):
+    soql_component = fields[field_name]
+    sf = API()
+    data = sf.query_ori(soql_component['object'], soql_component['field'], soql_component['conditions'])
+    data = data.to_dict('list')
+    return data
+
+@app.get('/eumir/')
+async def eumir(start_date_of_event: date | None = None,
+                end_date_of_event: date | None = None,
+                product_segment: Annotated[list[str] | None, Query()] = None,
+                rdc_code: Annotated[list[str] | None, Query()] = None,
+                rdc_clarifier: Annotated[list[str] | None, Query()] = None,
+                pc_code: Annotated[list[str] | None, Query()] = None,
+                result_code: Annotated[list[str] | None, Query()] = None,
+                conclusion_code: Annotated[list[str] | None, Query()] = None,
+                country_name: Annotated[list[str] | None, Query()] = None,
+                complaint_code: Annotated[str | None, Query(pattern='CN-\d\d\d\d\d\d$')] = None):
+    
+    object = 'CMPL123CME__Complaint__c A'
+    select_list = ['A.Id', 
+                   'A.Name', 
+                   'A.Product_Segment__c', 
+                   'A.Date_of_Event__c', 
+                   'A.Reportable_Country__c']
+    
+    temp_cond = []
+    if rdc_code: temp_cond.append("Code__c IN ('{0}')".format("', '".join(rdc_code)))
+    if rdc_clarifier: temp_cond.append("Clarifier__c IN ('{0}')".format("', '".join(rdc_clarifier)))
+    if temp_cond: select_list.append("(SELECT Id FROM RDC_Codes__r WHERE {0})".format(' AND '.join(temp_cond)))
+    
+    if pc_code: select_list.append("(SELECT Id FROM Patient_Codes__r WHERE Code__c IN ('{0}'))".format("', '".join(pc_code)))
+    
+    temp_cond = []
+    if result_code: temp_cond.append("Evaluation_Result_Code__r.Name IN ('{0}')".format("', '".join(result_code)))
+    if conclusion_code: temp_cond.append("Evaluation_Conclusion_Code__r.Name IN ('{0}')".format("', '".join(conclusion_code)))
+    if temp_cond: select_list.append("(SELECT Id FROM Engineering_Codings__r WHERE {0})".format(' AND '.join(temp_cond)))
+    
+    conditions = []
+    if start_date_of_event: conditions.append('Date_of_Event__c > {0}'.format(start_date_of_event))
+    if end_date_of_event: conditions.append('Date_of_Event__c < {0}'.format(end_date_of_event))
+    if product_segment: conditions.append("Product_Segment__c IN ('{0}')".format("', '".join(product_segment)))
+    if country_name: conditions.append("Reportable_Country__c IN ('{0}')".format("', '".join(country_name)))
+    if complaint_code: conditions.append("Name NOT IN ('{0}')".format(complaint_code))
+
+    select_statement = ', '.join(select_list)
+    conditions_statement = 'WHERE ' + ' AND '.join(conditions) if conditions else ''
+    soql = 'SELECT {0} FROM {1} {2}'.format(select_statement, object, conditions_statement)
+    soql = soql.strip()
+    soql = soql.replace(' ', '+')
+
+    sf = API()
+    api_link = sf.instance + sf.ext['query'] + '?q='
+    data = requests.get(api_link + soql, headers=sf.headers)
+    data = pd.DataFrame(data.json()['records'])
+    data = data.dropna()
+    data = data.to_dict('records')
+    return data
---
+++ sf_api.py
@@ -0,0 +1,161 @@
+from collections import OrderedDict
+from pathlib import Path
+from selenium import webdriver
+
+import pandas as pd
+
+import html
+import re
+import requests
+import webbrowser
+
+from math import ceil
+
+class Login():
+    def __init__(self):
+        self.api_version = 'v58.0'
+        self.instance = 'https://abbottecho.my.salesforce.com'
+        self._endpoint = self.instance + f'/services/data/{self.api_version}/'
+        self._file_session_id = '_session_id.txt'
+        self.session_id = self._check_session_id()
+        
+    def dev_guide(self):
+        link = 'https://developer.salesforce.com/docs/atlas.en-us.244.0.api_rest.meta/api_rest/intro_rest.htm'
+        webbrowser.open(link)
+        
+    def _check_session_id(self):
+        session_id = self._read_session_id()
+        r = requests.get(self._endpoint, headers={'Authorization': 'Bearer ' + session_id})
+        if r.status_code != 200:
+            session_id = self._get_session_id()
+        return session_id
+    
+    def _read_session_id(self):
+        if Path(self._file_session_id).is_file() == True:
+            with open (self._file_session_id, 'r') as file:  
+                session_id = file.readline()
+        else:
+            session_id = self._get_session_id()
+        return session_id
+      
+    def _get_saml_link(self):
+        pattern = re.compile('https://abbottecho.my.salesforce.com/saml/[a-zA-Z0-9!@#$%^&*()_+-={}:;<>,.?/]*')
+        html_doc = requests.get(self.instance).text
+        html_doc = html.unescape(html_doc)
+        saml_link = re.findall(pattern, html_doc)[0]
+        return saml_link
+        
+    def _get_session_id(self):
+
+        saml_link = self._get_saml_link()
+        options = webdriver.EdgeOptions()
+        options.add_argument('--headless')
+        driver = webdriver.Edge(options=options)
+        driver.implicitly_wait(10)
+        driver.get(saml_link)
+        
+        for i in range(0, 2000):
+            cookies = [cookie for cookie in driver.get_cookies() if 'sid' in cookie.values()]
+            if len(cookies) == 1: break
+        
+        session_id = cookies[0]['value']
+        driver.close()
+        
+        with open(self._file_session_id, 'w') as file:  
+            file.write(session_id)
+            
+        return session_id
+    
+class API(Login):
+    def __init__(self):
+        super().__init__()
+        self.headers = {'Authorization': 'Bearer ' + self.session_id}
+        self.ext = OrderedDict(sorted(self._get_ext().items()))
+        self.sobjects = self._sobjects()
+        
+    def _get_ext(self):
+        r = requests.get(self._endpoint, headers=self.headers)
+        return r.json()   
+    
+    def _sobjects(self):
+        r = requests.get(self.instance + self.ext['sobjects'], headers=self.headers)
+        data = r.json()['sobjects']
+        df = pd.DataFrame(data)
+        df = df[['label', 'name']].sort_values(by='label').reset_index(drop=True)
+        return df
+
+    def sobjects_search(self, text):
+        return self.sobjects[self.sobjects['label'].str.lower().str.contains(text.lower())]
+    
+    def object_field(self, object_name, search=''):
+        r = requests.get(self.instance + self.ext['sobjects'] + f'/{object_name}/describe', headers=self.headers)
+        data = r.json()['fields']
+        df = pd.DataFrame(data)
+        df = df[['label', 'name', 'type', 'length', 'updateable']]
+        df = df.sort_values(by='label').reset_index(drop=True)
+        
+        if search != '':
+            df = df[df['label'].str.lower().str.contains(search.lower())]
+            
+        return df
+    
+    def query(self, object_name, fields_with_rename={}, conditions=[], more=1, rename=True):
+        if fields_with_rename == {}:
+            df = self.object_field(object_name)
+            fields_with_rename = df[['label', 'name']].set_index('name').to_dict()['label']
+        fields = [name for name in fields_with_rename.keys()]
+        
+        SOQL = ''
+        SOQL += 'SELECT ' + ', '.join(fields) + ' '
+        SOQL += f'FROM {object_name} '
+        if len(conditions) > 0: SOQL += 'WHERE ' + ' AND '.join(conditions) + ' '
+        
+        r = requests.get(self.instance + self.ext['query'] + '?q=' + SOQL.replace(' ', '+'), headers=self.headers)
+        data_json = r.json()
+        df = pd.DataFrame(data_json['records'])
+        total_data = data_json['totalSize']
+        data_size = len(df)
+        
+        if more == 0 or more > total_data/data_size:
+            total_read = ceil(total_data/data_size) - 1
+        elif more == 1:
+            total_read = 0
+        else:
+            total_read = more - 1
+            
+        for i in range(total_read):
+            r = requests.get(self.instance + data_json['nextRecordsUrl'], headers=self.headers)
+            data_json = r.json()
+            df = pd.concat([df, pd.DataFrame(data_json['records'])])
+            
+        df.drop('attributes', axis=1, inplace=True)
+        if rename == True: 
+            df = df.rename(columns=fields_with_rename)
+        
+        return df
+    
+    def query_ori(self, object_name, field, conditions=[]):    
+        SOQL = ''
+        SOQL += f'SELECT {field} '
+        SOQL += f'FROM {object_name} '
+        if len(conditions) > 0: SOQL += 'WHERE ' + ' AND '.join(conditions) + ' '
+        SOQL += f'GROUP BY {field} '
+        
+        r = requests.get(self.instance + self.ext['query'] + '?q=' + SOQL.replace(' ', '+'), headers=self.headers)
+        data_json = r.json()
+        df = pd.DataFrame(data_json['records'])
+        total_data = data_json['totalSize']
+        data_size = len(df)
+        total_read = ceil(total_data/data_size) - 1
+            
+        for i in range(total_read):
+            r = requests.get(self.instance + data_json['nextRecordsUrl'], headers=self.headers)
+            data_json = r.json()
+            df = pd.concat([df, pd.DataFrame(data_json['records'])])
+            
+        df.drop('attributes', axis=1, inplace=True)
+        df.sort_values(by=field, ignore_index=True, inplace=True)
+        df.rename(columns={field: 'records'}, inplace=True)
+        df.dropna(inplace=True)
+        
+        return df
